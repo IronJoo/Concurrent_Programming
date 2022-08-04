b6a910f07494b6029ebca314496c1af230c2562f
Joana Ferro <joanaferro92@gmail.com>
2022-08-04 14:05:33 +0100

    Merge

--- src/Aula6/ex4/Main.java
+++ src/Aula6/ex4/Main.java
@@ -2,15 +2,15 @@ package Aula6.ex4;
 
 public class Main { //acabar em casa
     public static void main(String[] args) {
-        PaiNatal paiNatal = new PaiNatal();
-
-        Elfo listaDeElfos[] = new Elfo[50];
-
-        for (int i = 0; i < 50; i++){
-            listaDeElfos[i] = new Elfo();
-        }
-        for (int i = 0; i < 50; i++){
-            listaDeElfos[i].start;
-        }
+//        PaiNatal paiNatal = new PaiNatal();
+//
+//        Elfo listaDeElfos[] = new Elfo[50];
+//
+//        for (int i = 0; i < 50; i++){
+//            listaDeElfos[i] = new Elfo();
+//        }
+//        for (int i = 0; i < 50; i++){
+//            listaDeElfos[i].start;
+//        }
     }
 }
